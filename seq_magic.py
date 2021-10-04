a32a6f6c8b3503f051a2832341f3229d8d71339c
Shtol <mikha.shtol@gmail.com>
2021-10-04 20:36:38 +0300

    Some improovment of code according to flake8

--- seq_magic.py
+++ seq_magic.py
@@ -1,72 +1,73 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[49]:
+# In[2]:
 
 
-def transcribe(seq,nucleo):
-    if nucleo=="dna":
-        d=dna_rnadict
+def transcribe(seq, nucleo):
+    if nucleo == "dna":
+        d = dna_rnadict
     else:
-        d=rna_dnadict
+        d = rna_dnadict
+        print("In fact it's a reverse transcription, seq is RNA")
     return "".join([d[seq[i]] for i in range(len(seq))])
+
 
 def reverse(seq):
     return seq[::-1]
 
-def complement(seq,nucleo):
-    if nucleo=="dna":
-        d=dna_dnadict
-    else:
-        d=rna_rnadict
-    return "".join([d[seq[i]] for i in range(len(seq))])
-    
-command=""
-rna_dnadict={"A":"T","U":"A","G":"C","C":"G"}
-rna_rnadict={"A":"U","U":"A","G":"C","C":"G"}
-dna_dnadict={"A":"T","T":"A","G":"C","C":"G"}
-dna_rnadict={"A":"U","T":"A","G":"C","C":"G"}
 
-while command!='exit':
-    command=input('Do: ')
-    if command=='exit':
+def complement(seq, nucleo):
+    if nucleo == "dna":
+        d = dna_dnadict
+    else:
+        d = rna_rnadict
+    return "".join([d[seq[i]] for i in range(len(seq))])
+
+
+command = ""
+rna_dnadict = {"A": "T", "U": "A", "G": "C", "C": "G"}
+rna_rnadict = {"A": "U", "U": "A", "G": "C", "C": "G"}
+dna_dnadict = {"A": "T", "T": "A", "G": "C", "C": "G"}
+dna_rnadict = {"A": "U", "T": "A", "G": "C", "C": "G"}
+
+while command != 'exit':
+    command = input('Do: ')
+    if command == 'exit':
         print("NOOOOOOOO!!!")
         break
-    raw_sequence=input("Seq: ")
-    mask=[char.islower() for char in raw_sequence]
-    sequence=raw_sequence.upper()
-    
-    dna, rna = 1,1
-    nucleo=""
+    raw_sequence = input("Seq: ")
+    mask = [char.islower() for char in raw_sequence]
+    sequence = raw_sequence.upper()
+    dna, rna = True, True
+    nucleo = ""
     for char in sequence:
         if char not in dna_dnadict:
-            dna=0
+            dna = False
         if char not in rna_rnadict:
-            rna=0
-            
-    if dna==1:
-        nucleo='dna'
-    elif rna==1:
-        nucleo='rna'
+            rna = False
+    if dna == True:
+        nucleo = 'dna'
+    elif rna == True:
+        nucleo = 'rna'
     else:
         print("not a nucleic acid")
         continue
 
-    res=''
+    res = ''
     if command == 'transcribe':
-        res=transcribe(sequence, nucleo)
-        res=''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
-    elif command=='reverse':
-        res=reverse(raw_sequence)
-    elif command=='complement':
-        res=complement(sequence, nucleo)
-        res=''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
-    elif command=="reverse complement":
-        res=complement(sequence, nucleo)
-        res=''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
-        res=reverse(res)
+        res = transcribe(sequence, nucleo)
+        res = ''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
+    elif command == 'reverse':
+        res = reverse(raw_sequence)
+    elif command == 'complement':
+        res = complement(sequence, nucleo)
+        res = ''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
+    elif command == "reverse complement":
+        res = complement(sequence, nucleo)
+        res = ''.join([res[i].lower() if mask[i] else res[i] for i in range(len(res))])
+        res = reverse(res)
     else:
         print("Unknown command")
-
     print(res)
 
