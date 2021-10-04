b6a7b23d68c62d1cc81c21b899bd4e53b4d73ae6
Shtol <mikha.shtol@gmail.com>
2021-10-04 21:59:15 +0300

    First converter commit

---
+++ Ci_Bq_converter.py
@@ -0,0 +1,20 @@
+def Ci_Bq(x):
+    print(x * 3700000000, 'Becqerels')
+
+
+def Bq_Ci(x):
+    print(x / 3700000000, 'Curie')
+
+
+while True:
+    number = float(input('Number: '))
+    unit1_unit2 = input('print units for conversion (Ci_Bq or Bq_Ci): ')
+    if unit1_unit2 == 'Ci_Bq':
+        Ci_Bq(number)
+    elif unit1_unit2 == 'Bq_Ci':
+        Bq_Ci(number)
+    else:
+        print('Sorry, unknown units')
+    if input('Type "stop" to stop it') == 'stop':
+        print("stopped")
+        break
