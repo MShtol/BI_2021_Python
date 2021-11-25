da44130b480e54cb33856e98d028898015a30fb6
Shtol <mikha.shtol@gmail.com>
2021-11-25 20:42:08 +0300

    code brush

--- re/re.py
+++ re/re.py
@@ -5,7 +5,7 @@ with open('references.txt') as f:
     lines = f.read()
 pattern = re.compile(r'[;\s](ftp[^;\s]+)')
 match = pattern.findall(lines)
-with open('ftps','w') as ftps:
+with open('ftps', 'w') as ftps:
     ftps.write('\n'.join(match))
 # 2 find all numbers
 with open('2430AD.txt') as file:
@@ -14,7 +14,7 @@ pattern = re.compile(r'\b[\d]+\.*\d*\b')
 all_numbers = pattern.findall(text)
 # 2430 1969 2430 2430 57 57 1970 3.68 35 460 2430
 # 3 find all words with A or a in it
-pattern = re.compile(r'\b\S*[aA]\S*\b') # A.D is provided without second dot
+pattern = re.compile(r'\b\S*[aA]\S*\b')  # A.D is provided without second dot
 all_a_words = pattern.findall(text)
 # len(all_a_words) == 983
 # 4 Find all wxclamation sentences
@@ -29,11 +29,11 @@ unique_words = set(all_words)
 distr = {}
 print(len(unique_words))
 for i in unique_words:
-    l = len(i)
+    length = len(i)
     if l in distr:
-        distr[l] += 1
+        distr[length] += 1
     else:
-        distr[l] = 1
+        distr[length] = 1
 plt.bar(distr.keys(), distr.values(), color='purple')
 plt.xticks(list(distr.keys()))
 plt.title('Unique word length distribution')
