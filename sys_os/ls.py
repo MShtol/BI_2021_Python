0d197d390741a111222d7f0043a407a6e83f819f
Shtol <mikha.shtol@gmail.com>
2021-12-15 23:45:32 +0300

    code brush

--- sys_os/ls.py
+++ sys_os/ls.py
@@ -12,18 +12,18 @@ def list_content(dir_path, flag):
     '''
     os.chdir(dir_path)
     all_files = os.listdir()
-    if flag: 
+    if flag:
         return all_files
     else:
         return [i for i in all_files if not i.startswith('.')]
-        
-    
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="""List information about the FILEs 
-                                     (the current directory by default)""")
+    parser = argparse.ArgumentParser(
+        description="""List information about the FILEs (the current directory by default)""")
     parser.add_argument('-a', '--all', action='store_true', help='do not ignore .* files')
     parser.add_argument('dir_path', help="dir path/name", nargs='?', type=str,
                         default=os.getcwd())
     args = parser.parse_args()
-    output = list_content(args.dir_path,args.all)
+    output = list_content(args.dir_path, args.all)
     sys.stdout.write(' '.join(output))
